6374210b6befdcda973d31bc10f9335fa73bf888
abxxbo <devavcado@gmail.com>
2022-07-13 22:42:02 -0500

    remove unhandled interrupt testing

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -10,10 +10,7 @@
 void kernel_main(){
 	printf("Hello World.\n");
 	init_dt();
-	printf("GDT/IDT Enabled! Testing...\n\n");
-
-	asm("int $0x4");
-	asm("int $0xf");
+	printf("GDT/IDT Enabled!\n\n");
 
 	asm("sti");
 	// init_timer(50);
