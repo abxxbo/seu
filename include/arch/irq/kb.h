ff07da5f56a4ea6213bf2ec94fd6fc02ee9b8e39
abxxbo <devavcado@gmail.com>
2022-07-17 14:07:52 -0500

    keyboard driver does more than just print

--- include/arch/irq/kb.h
+++ include/arch/irq/kb.h
@@ -4,9 +4,67 @@
 #include <stdint.h>
 #include <arch/isr.h>
 
+const char sc_table[] ={
+	0, 0, '1', '2',
+	'3', '4', '5', '6',
+	'7', '8', '9', '0',
+	'-', '=', 0, 0,
+	'q', 'w', 'e', 'r',
+	't', 'y', 'u', 'i',
+	'o', 'p', '[', ']',
+	0, 0, 'a', 's',
+	'd', 'f', 'g', 'h',
+	'j', 'k', 'l', ';',
+	'\'', '`', 0, '\\',
+	'z', 'x', 'c', 'v',
+	'b', 'n', 'm', ',',
+	'.', '/', 0, '*',
+	0, ' '
+};
+
+
+int lshift = 0;
+int rshift = 0;
+
 void kbd_handler(registers_t regs){
-	uint8_t k = inb(0x60);
-	printf("Keyboard input achieved\n");
+	uint8_t k = inb(0x60); // ask CPU for port 0x60 -- keyboard
+	uint8_t c = 0;				 // we look up the chracter in the lookup table
+	if(k < 0x3a){
+		c = sc_table[k];
+	}
+
+	// check some cases
+	if(c != 0){
+		// check shift
+		switch(lshift | rshift){
+			case 1: // capitalized letters
+				putc(c - 32);
+				break;
+			case 0: // lowercase
+				putc(c);
+				break;
+		}
+	} else {
+		// other
+		switch(k){
+			case 0x2a: // left shift, pressed
+				lshift = 1;
+				break;
+			case 0xaa: // left shift, released
+				lshift = 0;
+
+			// the same thing for the right shift
+			case 0x36:
+				rshift = 1;
+				break;
+			case 0xb6:
+				rshift = 0;
+				break;
+
+			// newline
+			case 0x9c: printf("\n");
+		}
+	}
 }
 
 void init_kbd(){
