cafcc80d8573948c707ae4e33b955589dec88b2a
abxxbo <devavcado@gmail.com>
2022-07-17 19:00:52 -0500

    get fb addr

--- include/mem/asm/vesa.asm
+++ include/mem/asm/vesa.asm
@@ -1,15 +1,17 @@
 section .bss
-[global vbe_mode_info_structure]
+[global vbe_info_structure]
 vbe_info_structure:
 	.table_data: resb 512-4	;; vbe mode info for mode 0x4118
 
 section .text
+MODE equ 0x4105
+
 vbe_init:
 	xor ax, ax
 	mov es, ax
 	
 	mov ax, 0x4f01
-	mov cx, 0x4118
+	mov cx, MODE
 	mov di, vbe_info_structure
 	int 0x10
 
@@ -18,7 +20,7 @@ vbe_init:
 
 	;; since no error occurred, we can just set the video mode
 	mov ax, 0x4f02
-	mov bx, 0x4118
+	mov bx, MODE
 	int 0x10
 	ret;; no error
 
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -7,19 +7,14 @@
 #include <mem/paging.h>
 #include <mem/mmap/mmap.h>
 
+#include <video/vesa.h>
+
 void kernel_main(){
 	// set the first line to a light blue/cyan color
 	for(int i = 0; i <= 80; i++) wch_pos(0x20, 0x9, 0x9, i, 0);
-	setup_pg();
-	printf("\nHello, paging world\n");
 
-	printf("\n\n==== MMAP ENTRIES ====\n");
-	g_use_mem();
-
-	// initialize the interrupts
-	init_idt();
-	init_kbd();
-	init_cmos();
+	// setts 
+	volatile uint32_t *fb = (uint32_t *)((uint32_t) vbe_info_structure.framebuffer);
 
 	for(;;) asm("hlt");
 }
